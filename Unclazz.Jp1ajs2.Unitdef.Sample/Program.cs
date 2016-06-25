499efc3157127c47d1adecb4996179d5ec6d3315
mizuky fujitani <mizuky fujitani>
2016-06-25 23:47:02 +0900

    サンプルコードを修正

--- Unclazz.Jp1ajs2.Unitdef.Sample/Program.cs
+++ Unclazz.Jp1ajs2.Unitdef.Sample/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Unclazz.Jp1ajs2.Unitdef;
+using Unclazz.Jp1ajs2.Unitdef.Parser;
+using Unclazz.Jp1ajs2.Unitdef.Query;
 
 namespace Unclazz.Jp1ajs2.Unitdef.Sample
 {
@@ -11,8 +14,30 @@ namespace Unclazz.Jp1ajs2.Unitdef.Sample
     {
         static void Main(string[] args)
         {
-            IUnit u = Unit.FromString("unit=XXXX0000,,,;{ty=g;cm=foo;}")[0];
-            System.Console.WriteLine(u);
+            IUnit u = Unit.FromString(
+                "unit=XXXX0000,,,;\n" +
+                "{   ty=n;\n" +
+                "    cm=\"comment text...\";\n" +
+                "    unit=XXXX1000,,,;\n" +
+                "    {   ty=n;\n" +
+                "        unit=XXXX1100,,,;{ty=pj;sc=\"x:\\path\\to\\script\";prm=\"param list\";tho=0;}\n" +
+                "        unit=XXXX1200,,,;{ty=j;sc=\"/path/to/script\";prm=\"param list\";tho=1;}\n" +
+                "    }\n" +
+                "    unit=XXXX2000,,,;{ty=j;sc=\"/path/to/script\";tho=2;}\n" +
+                "}");
+
+            // 直属の下位ユニットのうち名前が1000で終わるものすべて
+            IEnumerable<IUnit> childrenNameEndsWith1000 = u.Query(Q.Children.NameEndsWith("1000"));
+            // ...そのうち1件だけ（存在しない場合は例外をスローする）
+            IUnit child0NameEndsWith1000 = u.Query(Q.Children.NameEndsWith("1001").One());
+
+            // 直属・非直属の下位ユニットのうち種別がUNIXジョブであるもののscパラメータすべて
+            IEnumerable<IParameter> paramsScOfDescendantsTypeIsUnixJob = u.Query(Q
+                .Descendants.TypeIs(UnitType.UnixJob).TheirParameters.NameIs("sc"));
+            // ...そのうち1件だけ（存在しない場合はnullを返す）
+            IParameter param0ScOfDescendantsTypeIsUnixJob = u.Query(Q
+                .Descendants.TypeIs(UnitType.UnixJob)
+                .TheirParameters.NameIs("sc").One(true));
         }
     }
 }
