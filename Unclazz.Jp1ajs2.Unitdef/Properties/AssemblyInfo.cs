750011f2cc193905ec720c8f5d55f32296b9ca7f
mizuky fujitani <mizuki.fujitani@gmail.com>
2018-05-27 17:01:04 +0900

    NuGetパッケージのための情報を更新

--- Unclazz.Jp1ajs2.Unitdef/Properties/AssemblyInfo.cs
+++ Unclazz.Jp1ajs2.Unitdef/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("unclazz.org")]
 [assembly: AssemblyProduct("Unclazz.Jp1ajs2.Unitdef")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
