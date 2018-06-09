4490c13683f04c7839ff70673441a8a8c01e4429
mizuky fujitani <mizuki.fujitani@gmail.com>
2018-06-09 09:49:25 +0900

    AttributesParserのテストケース追加とバグフィックス

--- Unclazz.Jp1ajs2.Unitdef.Test/Parser/UnitParser2.AttributesParserTest.cs
+++ Unclazz.Jp1ajs2.Unitdef.Test/Parser/UnitParser2.AttributesParserTest.cs
@@ -13,7 +13,8 @@ namespace Unclazz.Jp1ajs2.Unitdef.Test.Parser
         {
             // Arrange
             var p = new UnitParser2.AttributesParser();
-            var i = Reader.From("unit=foo,bar,baz,123;...", logAppender: Console.WriteLine);
+            var i = Reader.From("unit=foo,bar,baz,123;...");
+            //                   012345678901234567890123
 
             // Act
             var r = p.Parse(i);
@@ -21,6 +22,119 @@ namespace Unclazz.Jp1ajs2.Unitdef.Test.Parser
             // Assert
             Assert.That(r.Successful, Is.True);
             Assert.That(r.Capture.UnitName, Is.EqualTo("foo"));
+            Assert.That(r.Capture.PermissionMode, Is.EqualTo("bar"));
+            Assert.That(r.Capture.Jp1UserName, Is.EqualTo("baz"));
+            Assert.That(r.Capture.ResourceGroupName, Is.EqualTo("123"));
+            Assert.That(i.Position.Index, Is.EqualTo(21));
+        }
+        [Test]
+        public void Parse_Case02()
+        {
+            // Arrange
+            var p = new UnitParser2.AttributesParser();
+            var i = Reader.From("unit=foo,,,;...");
+            //                   012345678901234567890123
+
+            // Act
+            var r = p.Parse(i);
+
+            // Assert
+            Assert.That(r.Successful, Is.True);
+            Assert.That(r.Capture.UnitName, Is.EqualTo("foo"));
+            Assert.That(r.Capture.PermissionMode, Is.EqualTo(string.Empty));
+            Assert.That(r.Capture.Jp1UserName, Is.EqualTo(string.Empty));
+            Assert.That(r.Capture.ResourceGroupName, Is.EqualTo(string.Empty));
+            Assert.That(i.Position.Index, Is.EqualTo(12));
+        }
+        [Test]
+        public void Parse_Case03()
+        {
+            // Arrange
+            var p = new UnitParser2.AttributesParser();
+            var i = Reader.From("unit=foo,,;...");
+
+            // Act
+            var r = p.Parse(i);
+
+            // Assert
+            Assert.That(r.Successful, Is.True);
+            Assert.That(r.Capture.UnitName, Is.EqualTo("foo"));
+            Assert.That(r.Capture.PermissionMode, Is.EqualTo(string.Empty));
+            Assert.That(r.Capture.Jp1UserName, Is.EqualTo(string.Empty));
+            Assert.That(r.Capture.ResourceGroupName, Is.EqualTo(string.Empty));
+        }
+        [Test]
+        public void Parse_Case04()
+        {
+            // Arrange
+            var p = new UnitParser2.AttributesParser();
+            var i = Reader.From("unit=foo,;...");
+
+            // Act
+            var r = p.Parse(i);
+
+            // Assert
+            Assert.That(r.Successful, Is.True);
+            Assert.That(r.Capture.UnitName, Is.EqualTo("foo"));
+            Assert.That(r.Capture.PermissionMode, Is.EqualTo(string.Empty));
+            Assert.That(r.Capture.Jp1UserName, Is.EqualTo(string.Empty));
+            Assert.That(r.Capture.ResourceGroupName, Is.EqualTo(string.Empty));
+        }
+        [Test]
+        public void Parse_Case05()
+        {
+            // Arrange
+            var p = new UnitParser2.AttributesParser();
+            var i = Reader.From("unit=foo;...");
+
+            // Act
+            var r = p.Parse(i);
+
+            // Assert
+            Assert.That(r.Successful, Is.True);
+            Assert.That(r.Capture.UnitName, Is.EqualTo("foo"));
+            Assert.That(r.Capture.PermissionMode, Is.EqualTo(string.Empty));
+            Assert.That(r.Capture.Jp1UserName, Is.EqualTo(string.Empty));
+            Assert.That(r.Capture.ResourceGroupName, Is.EqualTo(string.Empty));
+        }
+        [Test]
+        public void Parse_Case11()
+        {
+            // Arrange
+            var p = new UnitParser2.AttributesParser();
+            var i = Reader.From("unit=,,,;...");
+
+            // Act
+            var r = p.Parse(i);
+
+            // Assert
+            Assert.That(r.Successful, Is.False);
+        }
+        [Test]
+        public void Parse_Case12()
+        {
+            // Arrange
+            var p = new UnitParser2.AttributesParser();
+            var i = Reader.From("unit=,bar,baz,123;...");
+
+            // Act
+            var r = p.Parse(i);
+
+            // Assert
+            Assert.That(r.Successful, Is.False);
+        }
+        [Test]
+        public void Parse_Case13()
+        {
+            // Arrange
+            var p = new UnitParser2.AttributesParser();
+            var i = Reader.From("unit=,bar,baz,123,456;...");
+
+            // Act
+            var r = p.Parse(i);
+
+            // Assert
+            Assert.That(r.Successful, Is.False);
         }
 	}
 }
--- Unclazz.Jp1ajs2.Unitdef/Parser/UnitParser2.AttributesParser.cs
+++ Unclazz.Jp1ajs2.Unitdef/Parser/UnitParser2.AttributesParser.cs
@@ -8,15 +8,15 @@ namespace Unclazz.Jp1ajs2.Unitdef.Parser
         {
             internal AttributesParser()
             {
-                var attr = CharsWhileIn(CharClass.Not(CharClass.AnyOf(",;")), min: 1).Repeat().Capture();
-                var attrCsv = attr.Repeat(min: 1, max: 4, sep: ',');
-                var unitAttrCsv = Keyword("unit=").Then(attrCsv).Then(';');
+                var name = CharsWhileIn(CharClass.Not(CharClass.AnyOf(",;"))).Capture();
+                var other = Char(',').Then(CharsWhileIn(CharClass.Not(CharClass.AnyOf(",;")), min: 0).Capture());
+                var unitAttrCsv = Keyword("unit=").Then(name).Then(other.Repeat(max: 3)).Then(';');
                 inner = unitAttrCsv.Map(arg => 
                 {
-                    var attr0 = arg[0];
-                    var attr1 = arg.Count < 2 ? string.Empty : arg[1];
-                    var attr2 = arg.Count < 3 ? string.Empty : arg[2];
-                    var attr3 = arg.Count < 4 ? string.Empty : arg[3];
+                    var attr0 = arg.Item1;
+                    var attr1 = arg.Item2.Count < 1 ? string.Empty : arg.Item2[0];
+                    var attr2 = arg.Item2.Count < 2 ? string.Empty : arg.Item2[1];
+                    var attr3 = arg.Item2.Count < 3 ? string.Empty : arg.Item2[2];
                     return Attributes.OfValues(attr0, attr1, attr2, attr3);
                 });
             }
