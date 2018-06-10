9f60d97d6dec053bb157d22a486c675523d2a856
mizuky fujitani <mizuki.fujitani@gmail.com>
2018-06-10 22:39:51 +0900

    NuGetパッケージのための情報を更新

--- Unclazz.Jp1ajs2.Unitdef/Properties/AssemblyInfo.cs
+++ Unclazz.Jp1ajs2.Unitdef/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.7.0.0")]
-[assembly: AssemblyFileVersion("1.7.0.0")]
+[assembly: AssemblyVersion("1.7.1.0")]
+[assembly: AssemblyFileVersion("1.7.1.0")]
 
 [assembly: InternalsVisibleTo("Test.Unclazz.Jp1ajs2.Unitdef")]
